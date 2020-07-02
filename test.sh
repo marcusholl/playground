91017478960496949c0a79ed7975356d4f955ad9
Marcus Holl <marcus.holl@sap.com>
2020-07-02 11:41:56 +0200

    Add some ls

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "Hello World"
+ls -lah
 
 curl -L --output piper https://github.com/SAP/jenkins-library/archive/v1.50.0.tar.gz
 chmod +x piper
+ls -lah
 ./piper version
 echo "... done"
