3ed3472ed643f4bb775889008755465fd9e52265
Marcus Holl <marcus.holl@sap.com>
2020-07-02 12:08:42 +0200

    update piper url

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ ls -lah
 
 cat /etc/os-release
 
-curl -L --output piper https://github.com/SAP/jenkins-library/archive/v1.50.0.tar.gz
+curl -L --output piper https://github.com/SAP/jenkins-library/releases/download/v1.50.0/piper
 chmod +x piper
 ls -lah
 ./piper version
