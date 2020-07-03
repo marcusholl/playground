c64d7a5a11e7c5e391a943259b1e806f6ae56853
Marcus Holl <marcus.holl@sap.com>
2020-07-03 16:27:33 +0200

    run docker hello world

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ ls -lah
 cat /etc/os-release
 
 docker --version
+docker run hello-world
 
 curl -L --output piper https://github.com/SAP/jenkins-library/releases/download/v1.50.0/piper
 chmod +x piper
