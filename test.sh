79c3adc82079201ee897871595ba3208e43517e6
Marcus Holl <marcus.holl@sap.com>
2020-07-03 16:32:40 +0200

    run our cf docker image

--- test.sh
+++ test.sh
@@ -7,6 +7,7 @@ cat /etc/os-release
 
 docker --version
 docker run hello-world
+docker run ppiper/cf-cli cf version
 
 curl -L --output piper https://github.com/SAP/jenkins-library/releases/download/v1.50.0/piper
 chmod +x piper
