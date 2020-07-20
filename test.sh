d924c343bbfec856e65db33344c4e73db8422fb3
Marcus Holl <marcus.holl@sap.com>
2020-07-20 12:04:12 +0200

    wip

--- test.sh
+++ test.sh
@@ -10,6 +10,9 @@ echo "MySecretDummy is"
 echo $mySecretDummy
 echo "========"
 
+echo "ENV"
+env
+
 docker --version
 
 #curl -L --output piper https://github.com/SAP/jenkins-library/releases/download/v1.50.0/piper
